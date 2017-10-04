3f95c7f13440405ff826af667eaa5f75d453d285
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2017-10-04 19:37:48 +0200

    Updated Layer Structure

--- Layer.h
+++ Layer.h
@@ -3,23 +3,12 @@
 #include "include.h"
 #include "Node.h"
 
-enum class LayerType {
-	INPUT,
-	HIDDEN,
-	OUTPUT,
-	UNDEFINED
-};
-
 class Layer
 {
 public:
-	LayerType type;
 	vector<Node> nodes;
 
 	Layer();
-	Layer(LayerType type);
-	Layer(LayerType type, int nodes);
-
-private:
-	void initNodes(int nodeCount);
+	Layer(vector<Node> nodes);
+	Layer(int nodes);
 };
