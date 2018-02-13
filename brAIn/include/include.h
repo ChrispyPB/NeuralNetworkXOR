7a6ebee9a3c9596a96848bc7aef9bf4d647224c6
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-02-13 15:32:26 +0100

    added json library

--- brAIn/include/include.h
+++ brAIn/include/include.h
@@ -5,10 +5,13 @@
 #include <tuple>
 #include <initializer_list>
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <limits>
 #include <ctime>
 #include <cstdlib>
+
+#include "json.hpp"
 
 #undef max
 #undef min
